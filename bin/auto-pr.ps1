b95a2131623ab621a5c78db091eef45f20015ea0
ViMx <55943111+vim64@users.noreply.github.com>
2025-05-30 00:01:33 +0200

    Update auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "<vim64>/<scoop-vim64>:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
